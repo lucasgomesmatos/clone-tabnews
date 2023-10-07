f6c2f6a1b8a5c185aed397b172b0c1f595aa63f6
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-10-07 11:40:21 -0300

    adionado o novo endpoint

--- models/calculadora.ts
+++
@@ -1,5 +0,0 @@
-function somar(argumento1: number, argumento2: number) {
-  return argumento1 + argumento2;
-}
-
-export { somar };
--- tests/calculadora.spec.ts
+++
@@ -1,7 +0,0 @@
-import { somar } from "../models/calculadora";
-
-test("Somar 2 + 2 deverá retornar 4", () => {
-  const resultado = somar(2, 2);
-
-  expect(somar(2, 2)).toBe(resultado);
-});
