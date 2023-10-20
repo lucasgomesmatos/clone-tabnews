43a8c732db08ae56b39737c26008c739031c8280
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-10-19 22:31:41 -0300

    adicionando pg

---
+++ infra/database.ts
@@ -0,0 +1,12 @@
+import { Client } from "pg";
+async function query(queryObject: string) {
+  const client = new Client();
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,4 +1,8 @@
+import database from "../../../../infra/database";
+
 test("GET to /api/v1/status should return 200", async () => {
+  console.log(database);
+
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
