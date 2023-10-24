1cfa09148ddad7d6b7ab54633e765e97d76d436f
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-10-23 23:43:00 -0300

    select

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,3 +1,8 @@
-export default function status(req, res) {
+import database from "../../../../infra/database";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   res.status(200).json({ status: "ok status" });
 }
