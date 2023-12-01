21fd4443d113627e8cbd5af9a0aa9f1beaee8e36
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-11-30 21:29:50 -0300

    feat: adicionando propriedades na rota de `status`

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,8 +1,23 @@
 import database from "infra/database";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ status: "ok status" });
+  const resultServerVersion = await database.query("SHOW server_version;");
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const resultOpenedConnections = await database.query(
+    "SELECT COUNT(*) used FROM pg_stat_activity;",
+  );
+  const serverVersion = resultServerVersion.rows[0].server_version;
+  const maxConnections = resultMaxConnections.rows[0].max_connections;
+  const OpenedConnections = resultOpenedConnections.rows[0].used;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: serverVersion,
+      max_connections: maxConnections,
+      opened_connections: OpenedConnections,
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,5 +1,25 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  const VERSION_DATABASE = 16;
+  const MAX_CONNECTIONS_DATABASE = 100;
+  const OPENED_CONNECTIONS_DATABASE = 9;
+
+  expect(responseBody.database).toBeDefined();
+  expect(Number(responseBody.database.version)).toEqual(VERSION_DATABASE);
+  expect(Number(responseBody.database.max_connections)).toEqual(
+    MAX_CONNECTIONS_DATABASE,
+  );
+  expect(Number(responseBody.database.opened_connections)).toEqual(
+    OPENED_CONNECTIONS_DATABASE,
+  );
+
+  console.log("responseBody", responseBody);
 });
