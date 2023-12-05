aff0d544d5c155d94498854c08fed7bf96e9a252
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-12-05 19:58:31 -0300

    refatore

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -3,21 +3,24 @@ import database from "infra/database";
 export default async function status(req, res) {
   const updatedAt = new Date().toISOString();
 
-  const resultServerVersion = await database.query("SHOW server_version;");
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const resultMaxConnections = await database.query("SHOW max_connections;");
   const resultOpenedConnections = await database.query(
     "SELECT COUNT(*) used FROM pg_stat_activity;",
   );
-  const serverVersion = resultServerVersion.rows[0].server_version;
+
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
   const maxConnections = resultMaxConnections.rows[0].max_connections;
   const OpenedConnections = resultOpenedConnections.rows[0].used;
 
   res.status(200).json({
     updated_at: updatedAt,
-    database: {
-      version: serverVersion,
-      max_connections: maxConnections,
-      opened_connections: OpenedConnections,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnections,
+        opened_connections: OpenedConnections,
+      },
     },
   });
 }
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -8,18 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  const VERSION_DATABASE = 16;
-  const MAX_CONNECTIONS_DATABASE = 100;
-  const OPENED_CONNECTIONS_DATABASE = 9;
-
-  expect(responseBody.database).toBeDefined();
-  expect(Number(responseBody.database.version)).toEqual(VERSION_DATABASE);
-  expect(Number(responseBody.database.max_connections)).toEqual(
-    MAX_CONNECTIONS_DATABASE,
-  );
-  expect(Number(responseBody.database.opened_connections)).toEqual(
-    OPENED_CONNECTIONS_DATABASE,
-  );
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 
   console.log("responseBody", responseBody);
 });
