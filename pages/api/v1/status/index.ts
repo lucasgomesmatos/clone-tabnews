14dd33e77e6753052560c9ba280f4d04f833d433
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-12-15 21:38:45 -0300

    adicionando opened_connections da `banco de dados`

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -7,14 +7,15 @@ export default async function status(req, res) {
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
-  const resultOpenedConnections = await database.query(
-    "SELECT COUNT(*) used FROM pg_stat_activity;",
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
   );
 
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
-  const OpenedConnections = resultOpenedConnections.rows[0].used;
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
     updated_at: updatedAt,
@@ -22,7 +23,7 @@ export default async function status(req, res) {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: OpenedConnections,
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
